306c0b388b1c9cdee511363509cb5a9d6b9a41e4
seoyoung1024 <jjssyy0608@gmail.com>
2025-05-09 16:25:56 +0900

    up

--- worldcup/routes/main.js
+++ worldcup/routes/main.js
@@ -1,15 +1,19 @@
-
 const express = require("express");
 const margan = require("morgan");
 const fs = require("fs");
 const path = require("path");
 const mongoClient = require("mongodb").MongoClient;
+const env = require('dotenv').config({path:'../.env'})
 
 const app = express();
 app.use(margan("dev"));
 
+HOST = process.env.MongoDB_Hostname;
+USERNAME = process.env.MongoDB_Username;
+PASSWORD = process.env.MongoDB_Password;
+
 var db;
-var databaseUrl = "mongodb://52.221.19.6:27017";
+var databaseUrl = `mongodb://${USERNAME}:${PASSWORD}@${HOST}:27017`;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname, "./index.html");
